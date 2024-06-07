64840fbf051731024cc4c56c4b0a2ddd7fa789c7
POO <ariel.ashqui@epn.edu.ec>
2024-06-07 11:39:24 -0500

    Cambio Ariel

--- DatosQuemados/CuentaBancaria.java
+++ DatosQuemados/CuentaBancaria.java
@@ -1,4 +1,24 @@
 package DatosQuemados;
 
 public class CuentaBancaria {
+    int numeroCuenta;
+    double saldo;
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public CuentaBancaria(int numeroCuenta, double saldo){
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+    public void depositar(){
+        double monto;
+    }
+    public void retirar(){
+        double monto;
+    }
 }
---
+++ DatosQuemados/CuentaCorriente.java
@@ -0,0 +1,10 @@
+class CuentaCorriente extends CuentaBancaria{
+    double limiteDescubierto;
+
+    public CuentaCorriente(int numeroCuenta, double limiteDescubierto){
+        this.limiteDescubierto = limiteDescubierto;
+    }
+    public void retirar(){
+
+    }
+}
