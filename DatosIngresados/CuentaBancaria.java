717de5b00a24ced393ddc16cfa62deda57618ff7
POO <taty-1997-02@outlook.com>
2024-06-07 11:44:26 -0500

    Commit Evelyn

--- DatosIngresados/CuentaBancaria.java
+++ DatosIngresados/CuentaBancaria.java
@@ -1,4 +1,29 @@
 package DatosIngresados;
 
 public class CuentaBancaria {
+    private int numeroCuenta;
+    private double saldo;
+    public double monto;
+
+    public CuentaBancaria(int numeroCuenta){
+    this.numeroCuenta=numeroCuenta;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void depositar() {
+        System.out.println("El monto ingresado a depositar es" + monto);
+        monto=getSaldo()+monto;
+        System.out.println("Total de la cuenta: " + monto);
+    }
+    public void retirar() {
+        System.out.println("El monto ingresado a retirar es" + monto);
+        monto=getSaldo()-monto;
+        System.out.println("Total de la cuenta: " + monto);
+    }
 }
---
+++ DatosIngresados/Main.java
@@ -0,0 +1,9 @@
+package DatosIngresados;
+
+public class Main {
+    public static void main (String[] args){
+
+
+        CuentaBancaria persona1 =new CuentaBancaria()
+    }
+}
